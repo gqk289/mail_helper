b3ed5d64cd7f2d37508bbc9150680c7cf9c6f238
gqk289 <gqk289@gmail.com>
2015-10-07 15:32:34 +0800

    first push

---
+++ ptest/forum_public.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+
+import urllib,urllib2,re,time,thread,sqlite3,string
+import sys 
+from mail_test2 import mailtest
+
+class Spider_Model:
+
+    def __init__(self):
+        self.page = 1
+        self.pages = []
+        self.enable = False
+        self.conn = None
+        self.cursor = None
+
+        
+    def GetPage(self,page):
+        myUrl = "http://m.byr.cn/board/ParttimeJob"+"?p="+page
+        user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'   
+        headers = { 'User-Agent' : user_agent }   
+        req = urllib2.Request(myUrl, headers = headers)
+        myResponse = urllib2.urlopen(req)
+        myPage = myResponse.read()
+        unicodePage = myPage.decode("utf-8")
+
+        myItems = re.findall('<li.*?><div><a href="(.*?)">(.*?)</a>',unicodePage,re.S)
+        items = []
+        for item in myItems:
+            if not item[0].find('class')>=0:
+                items.append([item[0],item[1]])
+        return items
+
+    def LoadPage(self):
+        while self.enable:
+            if len(self.pages)<2:
+                try:
+                    myPage = self.GetPage(str(self.page))
+                    self.page += 1
+                    self.pages.append(myPage)
+                except BaseException,e:
+                    print e
+
+                else:
+                    time.sleep(1)
+
+    def GetEmail(self,myUrl):
+        user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'   
+        headers = { 'User-Agent' : user_agent }   
+        req = urllib2.Request(myUrl, headers = headers)
+        myResponse = urllib2.urlopen(req)
+        myPage = myResponse.read()
+        unicodePage = myPage.decode("utf-8")
+
+        myEmail = re.search('[a-zA-Z0-9](([a-zA-Z0-9]*\.[a-zA-Z0-9]*)|[a-zA-Z0-9]*)[a-zA-Z0-9]@([a-z0-9A-Z]+\.)+[a-zA-Z]{2,}',unicodePage,re.S)
+
+        return myEmail
+
+    
+
+    def ShowPage(self,nowPage,page):
+        self.conn = sqlite3.connect('forum.db')
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('create table if not exists forum_data (id INTEGER primary key, title varchar(20), attr varchar(20),email varchar(20))')
+
+        # 输入Email地址和口令:
+##        from_addr = raw_input('From: ')
+##        password = raw_input('Password: ')
+##        # 输入SMTP服务器地址:
+##        smtp_server = raw_input('SMTP server: ')
+##        # 输入收件人地址:
+##        to_addr = raw_input('To: ')
+##
+##        title = raw_input(u'邮件标题: ')
+##        content = raw_input(u'邮件正文: ')
+##        address = raw_input(u'附件地址: ')
+##        key_word = raw_input(u'关键词(以空格分隔): ')
+##        key_word = key_word.decode('utf-8')
+        #发送方邮箱地址
+        from_addr = 'xxx'
+##        to_addr = 'xxx'
+        #发送方邮箱密码
+        password = 'xxx'
+        #发送方邮箱smtp
+        smtp_server = 'xxx'
+        #邮件标题
+        title = 'xxxx'
+        #邮件正文
+        content = 'xxxx'
+        #附件路径
+        address = u'xxx'
+        #关键词 例子
+        key_word = u'猿题库 服务器'
+        words = key_word.split(" ")
+        
+        count = 1
+        for items in nowPage:
+            if page <= 3:
+                attr = items[0].split('ParttimeJob/')
+                article_id = string.atoi(attr[1])
+                article_title = items[1]
+                article_attr = 'http://m.byr.cn/article/ParttimeJob/'+attr[1]
+                myEmail = self.GetEmail(article_attr)
+                
+                is_want = True
+                if myEmail is None:
+                    page_email = 'null'
+                else:
+                    page_email = myEmail.group(0)
+                    to_addr = page_email
+                    if count<=10:
+                        for word in words:
+                            if word not in article_title:
+                                is_want = False
+                                break
+                        if is_want:
+                            self.cursor.execute("SELECT * FROM forum_data WHERE id = %d" %article_id)
+                            result = self.cursor.fetchone()
+                            if result is None:
+                                mailtest(from_addr, to_addr, article_title, content, address, smtp_server, password) 
+                                print u'第%d个' %count,article_title 
+                                count = count+1
+                                self.cursor.execute("insert or ignore into forum_data (id ,title, attr, email) values (?, ?, ?, ?)",(article_id, article_title, article_attr, page_email))
+                                self.conn.commit()
+##                print u'第%d页' %page,article_id, article_title, article_attr
+            else:
+                self.cursor.close()
+                self.conn.close()
+                self.enable = False
+                break
+
+    def Start(self):
+        self.enable = True
+        page = self.page
+
+        print u'正在加载请稍后'
+        
+        thread.start_new_thread(self.LoadPage,())
+
+        while self.enable:
+            if self.pages:
+                nowPage = self.pages[0]
+                del self.pages[0]
+                self.ShowPage(nowPage,page)
+                page += 1
+
+
+           
+
+
+print u'请按下回车开始自动投递简历：'
+
+raw_input(' ')
+myModel = Spider_Model()    
+myModel.Start() 
+  
+               
+
+    
+                
+                                 
+        
